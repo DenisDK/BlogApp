bae30a51a0d07ffa6092cc097c26f60b3c4b96bb
DenisDK <unworkdk@gmail.com>
2025-08-24 19:31:33 +0300

    Navbarr + Footer + Featured

---
+++ components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./themeToggle.module.css";
+
+const ThemeToggle = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.ball}></div>
+    </div>
+  );
+};
+
+export default ThemeToggle;
