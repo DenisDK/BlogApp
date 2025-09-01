3bc9840ce2073aa0360a9be6b16f947a3956060b
DenisDK <unworkdk@gmail.com>
2025-09-01 21:39:30 +0300

    Post with user info

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -4,51 +4,57 @@ import Menu from "../../../components/menu/Menu";
 import Image from "next/image";
 import Comments from "../../../components/comments/comments";
 
-const page = () => {
+const getData = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const { id } = params;
+
+  const data = await getData(id);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          </h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data.user.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>DenisDK</span>
+              <span className={styles.username}>{data?.user?.name}</span>
               <span className={styles.date}>29.08.2025</span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.desc}>
-            <p className={styles.desc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusantium libero numquam voluptatem dicta illum nemo voluptate
-              aspernatur necessitatibus at. Ut possimus omnis animi debitis,
-              quos ullam aliquid quasi laudantium iste?
-            </p>
-            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
-            <p className={styles.desc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusantium libero numquam voluptatem dicta illum nemo voluptate
-              aspernatur necessitatibus at. Ut possimus omnis animi debitis,
-              quos ullam aliquid quasi laudantium iste?
-            </p>
-            <p className={styles.desc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Accusantium libero numquam voluptatem dicta illum nemo voluptate
-              aspernatur necessitatibus at. Ut possimus omnis animi debitis,
-              quos ullam aliquid quasi laudantium iste?
-            </p>
-          </div>
+          <div
+            className={styles.desc}
+            dangerouslySetInnerHTML={{ __html: data?.desc || "" }}
+          />
           <div className={styles.comments}>
             <Comments />
           </div>
@@ -59,4 +65,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default SinglePage;
