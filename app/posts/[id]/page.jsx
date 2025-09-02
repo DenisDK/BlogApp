f93f2c5cd8ccecc009504c17eb83248d7bfe0552
DenisDK <unworkdk@gmail.com>
2025-09-02 22:46:57 +0300

    Client side data fetching

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -25,7 +25,7 @@ const SinglePage = async ({ params }) => {
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data.title}</h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
             {data?.user?.image && (
               <div className={styles.userImageContainer}>
@@ -38,8 +38,10 @@ const SinglePage = async ({ params }) => {
               </div>
             )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user?.name}</span>
-              <span className={styles.date}>29.08.2025</span>
+              <span className={styles.username}>
+                {data?.user?.name || "Unknown User"}
+              </span>
+              <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
@@ -52,11 +54,11 @@ const SinglePage = async ({ params }) => {
       <div className={styles.content}>
         <div className={styles.post}>
           <div
-            className={styles.desc}
+            className={styles.description}
             dangerouslySetInnerHTML={{ __html: data?.desc || "" }}
           />
-          <div className={styles.comments}>
-            <Comments />
+          <div className={styles.comment}>
+            <Comments postSlug={data.slug} />
           </div>
         </div>
         <Menu />
